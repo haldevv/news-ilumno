5681cb195e0f832ee595210e203a9a0e1f8cd097
Harold Yepes <harold.yepes@wihom.com.co>
2021-08-22 08:36:32 -0500

    Created initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,40 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Components
+
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+// Material Modules
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+const materialModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatSidenavModule
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     StoreModule.forRoot({}, {}),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ...materialModules
   ],
   providers: [],
   bootstrap: [AppComponent]
